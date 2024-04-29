eb5860878ee87e70496ddabc2c92e025ddb0a946
RasyaPrtma <rasyaputraq17@gmail.com>
2024-04-29 15:49:59 +0700

    Change Logic In Questions

--- src/Modules/Components/SingleForms.jsx
+++ src/Modules/Components/SingleForms.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from "react";
 import { UseForms } from "../Context/FormsContext";
 import { UseAuth } from "../Context/AuthContext";
